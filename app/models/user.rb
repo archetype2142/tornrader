b360b52d1728baf6924cbf27435dae6f4cba9c10
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-08 23:06:14 +0200

    Added new

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+         :rememberable
   
   has_many :item_lists
   has_many :items, through: :item_lists
