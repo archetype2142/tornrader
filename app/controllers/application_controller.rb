33ffaeab7a68653922fdb399cc1ef713231fa053
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-09 00:57:31 +0200

    Added homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || trades_path
+  end
 end
