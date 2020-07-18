3d4dd0cbe51591b3299facd5781e1d94143a88fd
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-18 04:25:28 +0200

    fix

--- app/workers/update_user_prices_worker.rb
+++ app/workers/update_user_prices_worker.rb
@@ -4,7 +4,7 @@ class UpdateUserPricesWorker
 
   def perform(user_id, add_all=false)
     user = User.find(user_id)
-    categories = user.categories
+    categories = user.categories.uniq
 
     if user.enable_global?
       if add_all
