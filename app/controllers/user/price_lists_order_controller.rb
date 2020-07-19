5f8d7199880acea11441255fc530e34640fd4df1
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-20 01:09:24 +0200

    Added

--- app/controllers/user/price_lists_order_controller.rb
+++ app/controllers/user/price_lists_order_controller.rb
@@ -34,8 +34,6 @@ class User::PriceListsOrderController < ApplicationController
 
   def auto_update_user_prices
     UpdateUserPricesWorker.perform_async(current_user.id)
-    puts "user_pricelist_#{current_user.username}"
-    Rails.cache.delete("user_pricelist_#{current_user.username}")
 
     redirect_to user_price_lists_order_index_path, flash: { success: "Please wait a few moments for prices to update"}
   end
