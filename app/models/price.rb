a85bc8187e371b0cc40356c0d5fa9f28934147e6
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-21 01:35:15 +0200

    dedduped

--- app/models/price.rb
+++ app/models/price.rb
@@ -9,3 +9,14 @@ class Price < ApplicationRecord
   enum pricing_rules: %w[percentage fixed]
   # validates :amount, numericality: { only_integer: true, greater_than: 0 }
 end
+
+# PRICEDEDUPE
+# User.all.each do |u|
+#   dups = u.prices.group(:item_id).having('count("item_id") > 1').count
+
+#   dups.each do |key, value|
+#     duplicates = u.prices.where(item_id: key)[1..value-1]
+#     puts "#{key} = #{duplicates.count}"
+#     duplicates.each(&:destroy)
+#   end
+# end
