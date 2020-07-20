3f6a75da8a6ae0b18da42d3c046bb76573995861
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-20 21:30:32 +0200

    fix

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -8,9 +8,10 @@ class LineItem < ApplicationRecord
   def update_total
     user_id = self.trade.user.id
 
-    total_price = self.item.nil? ? 0 : (
-      self.prices.last.amount * self.quantity
-    )
+    total_price = self.prices.any? ? (
+      self.prices.first.amount * self.quantity
+    ) : 0
+    
     if self.total != total_price
       self.total = total_price 
     end
@@ -19,13 +20,9 @@ class LineItem < ApplicationRecord
   def update_total_manual
     user_id = self.trade.user.id
 
-    total_price = self.item.nil? ? 0 : (
+    total_price = self.prices.any? ? (
       self.prices.first.amount * self.quantity
-    )
-    puts self.prices.first.amount
-    puts self.quantity
-    puts total_price
-    puts self.total
+    ) : 0
 
     if self.total != total_price
       self.update!(total: total_price)
