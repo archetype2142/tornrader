d7ea32613fd6a057f733013df1a111bbe3e57172
archetype2142 <malhotraritwick2011@gmail.com>
2020-08-17 16:30:29 +0200

    fix typo

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -28,7 +28,7 @@ class Rack::Attack
       req.path.include?('NonExistence') ||
       req.path.include?('solr') ||
       req.path.include?('.well-known') ||
-      req.path.include?('.git') ||
+      req.path.include?('.git')
     end
   end
 end
