d5e583eaf64554ce62759d863ebb5c99d4c9fa74
archetype2142 <malhotraritwick2011@gmail.com>
2020-08-23 00:56:42 +0200

    fix

--- app/controllers/trade_settings_controller.rb
+++ app/controllers/trade_settings_controller.rb
@@ -4,12 +4,19 @@ class TradeSettingsController < ApplicationController
 
   def create
     user = current_user
-    if user.update(forum_url: params['user']['forum_url'])
-      flash = { success: "Added successfully!" }
+
+    if user.update(permitted_params)
+      flash = { success: "Saved successfully!" }
     else
       flash = { error: user.errors }
     end
 
     redirect_to trade_settings_path, flash: flash
+  end
+
+  private
+
+  def permitted_params
+    params.require(:user).permit(:forum_url, :short_url)
   end
 end
