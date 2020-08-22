fa608973108ff28b6e9c5cc39d260837e1db0ad7
archetype2142 <malhotraritwick2011@gmail.com>
2020-08-23 01:30:54 +0200

    Added set pricing

--- app/controllers/copy_trader_controller.rb
+++ app/controllers/copy_trader_controller.rb
@@ -38,14 +38,21 @@ class CopyTraderController < ApplicationController
         )
 
         trade.update!(
-          short_url: url_maker(
-            trade_url(trade)
-          )
+          short_url: current_user.shortened? ? url_maker(trade_url(trade)) : trade_url(trade)
         )
 
         trade.line_items.destroy_all if trade.line_items.any?
-                
-        items_list.each do |item|
+        all_items = items_list
+
+        # 1048 and 1049 are item ids for plusie set and flower set respectively
+        if user_prices.where(item_id: [1048, 1049]).count == 2
+          flowers_checked = flower_set_exists?(items_list, user_prices.find_by(item_id: 1049))
+          plushie_checked = plushie_set_exists?(flowers_checked, user_prices.find_by(item_id: 1048))
+          all_items = plushie_checked
+        end
+
+        all_items.each do |item|
+          puts item.inspect
           trade.line_items.create!(
             prices: [item["price_object"]],
             quantity: item["quantity"]
@@ -108,4 +115,57 @@ class CopyTraderController < ApplicationController
     JSON.parse(response.body)["short_url"]
   end
 
+  def plushie_set_exists?(items, price)
+    plushies ||= ["Sheep Plushie", "Teddy Bear Plushie", "Kitten Plushie",
+    "Jaguar Plushie", "Wolverine Plushie", "Nessie Plushie",
+    "Red Fox Plushie", "Monkey Plushie", "Chamois Plushie",
+    "Panda Plushie", "Lion Plushie", "Camel Plushie"]
+    new_items = items
+    if (plushies - new_items.pluck("name")).empty?
+      found_plushies = []
+      
+      plushies.each do |plush|
+        found_plushies.push(new_items.select{ |i| i["name"].to_s == plush })
+      end
+      found_plushies.flatten!
+      new_items = new_items - found_plushies
+
+      sets_amount_found = found_plushies.pluck("quantity").min
+
+      set_items_after_removed = found_plushies.select { |plush| plush["quantity"] > sets_amount_found }
+      set_items_after_removed.each{ |i| i["quantity"] -= sets_amount_found}
+      
+      new_items.push({"name" => "Plushie Set", "quantity" => sets_amount_found, "price_object" => price})
+      new_items.push(set_items_after_removed)
+      new_items.flatten!
+    end
+    return new_items
+  end
+
+  def flower_set_exists?(items, price)
+    flowers ||= ["African Violet", "Banana Orchid", "Cherry Blossom", 
+    "Ceibo Flower", "Crocus", "Dahlia", "Edelweiss", "Heather", 
+    "Orchid", "Peony", "Tribulus Omanense"]
+
+    new_items = items
+
+    if (flowers - new_items.pluck("name")).empty?
+      found_flowers = []
+      
+      flowers.each do |plush|
+        found_flowers.push(new_items.select{ |i| i["name"].to_s == plush })
+      end
+      found_flowers.flatten!
+      new_items = new_items - found_flowers
+
+      sets_amount_found = found_flowers.pluck("quantity").min
+      set_items_after_removed = found_flowers.select { |plush| plush["quantity"] > sets_amount_found }
+      set_items_after_removed.each{ |i| i["quantity"] -= sets_amount_found}
+      
+      new_items.push({"name" => "Flower Set", "quantity" => sets_amount_found, "price_object" => price})
+      new_items.push(set_items_after_removed)
+      new_items.flatten!
+    end
+    return new_items
+  end
 end
