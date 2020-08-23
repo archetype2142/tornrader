47f30759df0e8fb38758cd22fa7efb08ce6d738d
archetype2142 <malhotraritwick2011@gmail.com>
2020-08-23 03:19:15 +0200

    stat mistake

--- app/controllers/user/statistics_controller.rb
+++ app/controllers/user/statistics_controller.rb
@@ -5,7 +5,8 @@ class User::StatisticsController < ApplicationController
   def index
     @all_trades ||= current_user.trades.group_by_day(:created_at).count
     @all_trades_count ||= current_user.trades.count
-    @sellers ||= Trade.all.pluck(:seller)
+    @sellers ||= current_user.trades.all.pluck(:seller)
+    
     @top_seller ||= @sellers.group_by{|i| i}.max{|x,y| x[1].length <=> y[1].length}[0]
     @total ||= current_user.trades.pluck(:total).sum
     @traders_data ||= @sellers.group_by(&:itself).map do |k, v| 
