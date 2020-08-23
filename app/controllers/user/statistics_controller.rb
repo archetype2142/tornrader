95a2e9c7400020d552cacdba1a2740e176d6a1ed
archetype2142 <malhotraritwick2011@gmail.com>
2020-08-23 03:42:22 +0200

    fixing stats

--- app/controllers/user/statistics_controller.rb
+++ app/controllers/user/statistics_controller.rb
@@ -6,15 +6,17 @@ class User::StatisticsController < ApplicationController
     @all_trades ||= current_user.trades.group_by_day(:created_at).count
     @all_trades_count ||= current_user.trades.count
     @sellers ||= current_user.trades.all.pluck(:seller)
-    
+
     @top_seller ||= @sellers.group_by{|i| i}.max{|x,y| x[1].length <=> y[1].length}[0]
+    
     @total ||= current_user.trades.pluck(:total).sum
+    
     @traders_data ||= @sellers.group_by(&:itself).map do |k, v| 
       next if v == 0
-      [k, v.length] 
-    end
+      {name: k, count: v.length}
+    end.sort_by { |hsh| hsh[:count] }.reverse!
+
+
+    @total_items = current_user.trades.map { |t| t.line_items.pluck(:quantity) }.flatten.sum
   end
 end
-
-
-
