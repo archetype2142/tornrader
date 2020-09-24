a21436a0be617af8c079c8d84cfb7b20ab92a134
archetype2142 <malhotraritwick2011@gmail.com>
2020-09-24 23:31:28 +0200

    added raven context

--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,16 @@ require 'dotenv/load'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Raven.configure do |config|
+  config.dsn = 'https://3a9d5dd9774f495f8ed5a1f39d93bd1f@o452740.ingest.sentry.io/5440619'
+end
+
 module TornTrader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.active_record.default_timezone = 'London'
+    config.filter_parameters << :password
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -19,3 +24,4 @@ module TornTrader
     # the framework and any gems in your application.
   end
 end
+
