f7acb819b54d3c1cde5612aebcce5c4b4ccded51
archetype2142 <malhotraritwick2011@gmail.com>
2020-09-24 23:34:38 +0200

    Added raven

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,6 @@ require 'dotenv/load'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Raven.configure do |config|
-  config.dsn = 'https://3a9d5dd9774f495f8ed5a1f39d93bd1f@o452740.ingest.sentry.io/5440619'
-end
-
 module TornTrader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,3 +1,8 @@
-Raven.configure do |config|
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+if Rails.env.production?
+  Raven.configure do |config|
+    config.dsn = 'https://3a9d5dd9774f495f8ed5a1f39d93bd1f@o452740.ingest.sentry.io/5440619'
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.environments = ["production"]
+    config.excluded_exceptions = []
+  end
 end
