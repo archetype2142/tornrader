72431bb59f72e6654234b1460d12ab84dedb720d
archetype2142 <malhotraritwick2011@gmail.com>
2020-09-26 18:07:37 +0200

    Added new  timing

---
+++ app/services/create_user_batches.rb
@@ -0,0 +1,1 @@
+create_user_batches.rb
--- config/clockwork.rb
+++ config/clockwork.rb
@@ -6,37 +6,54 @@ require_relative "../config/environment"
 require "clockwork"
 
 module Clockwork
+  infrequent_prices = Item.all.where(base_price: 0).ids
+  api_key = User.find_by(username: "archetype2142").torn_api_key
+  per_batch_time = 1.5
+  batch_size = 5
+
   configure do |config|
     config[:tz] = "Etc/UTC"
     config[:logger] = Logger.new Rails.root.join("log", "clockwork.log")
   end
 
-  api_key = User.find_by(username: "archetype2142").torn_api_key
+  every 45.minutes, "45-minutely" do
+    LowestPointPriceFetchWorker.perform_async(api_key)
+    
+    User.auto_updated.pluck(:id).each_slice(batch_size).each_with_index do |user_id, index|
+      UpdateUserPricesWorker.perform_at(
+        Time.now + (index * per_batch_time).minute,
+        user_id
+      )
+    end
+    GC.start
+  end
 
-  every 30.minutes, "thirty-minutely" do
-    total_items = (1..1065).to_a.each_slice(60).to_a
+  every 15.minutes, "fifteen-minutely" do
+    total_items = ((1..1065).to_a - infrequent_prices).each_slice(60).to_a
 
-    (0..17).to_a.each_with_index do |item_set, index|
+    (0..total_items.size).to_a.each_with_index do |item_set, index|
+      LowestPriceFetchWorker.perform_at(
+        Time.now + (index+1).minute,
+        total_items[item_set], 
+        api_key
+      )
+    end
+    GC.start
+  end
+
+  every 6.hours, "six-hourly" do
+    MarketValueFetchWorker.perform_async(api_key)
+  end
+
+  every 12.hours, "12-hourly" do
+    update_prices = infrequent_prices.each_slice(60).to_a
+
+    (0..update_prices.size).to_a.each_with_index do |item_set, index|
       LowestPriceFetchWorker.perform_at(
         Time.now + (index+1).minute, 
         total_items[item_set], 
         api_key
       )
     end
-  end
-
-  every 45.minutes, "forty-five-minutely" do
-    LowestPointPriceFetchWorker.perform_async(api_key)
-    
-    User.auto_updated.pluck(:id).each_with_index do |user_id, index|
-      UpdateUserPricesWorker.perform_at(
-        Time.now + (index+10).seconds,
-        user_id
-      )
-    end
-  end
-
-  every 6.hours, "six-hourly" do
-    MarketValueFetchWorker.perform_async(api_key)
   end
 end
