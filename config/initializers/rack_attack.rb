810f04cc822814a1a83f8fc19be9efd18f0bff3e
archetype2142 <malhotraritwick2011@gmail.com>
2020-09-27 16:48:21 +0200

    added 503 fix

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,36 +1,36 @@
 class Rack::Attack
-  Rack::Attack.blocklisted_response = lambda do |request|
-    # Using 503 because it may make attacker think that they have successfully
-    # DOSed the site. Rack::Attack returns 403 for blocklists by default
-    [ 503, {}, ['Server Error\n']]
-  end
+  # Rack::Attack.blocklisted_response = lambda do |request|
+  #   # Using 503 because it may make attacker think that they have successfully
+  #   # DOSed the site. Rack::Attack returns 403 for blocklists by default
+  #   [ 503, {}, ['Server Error\n']]
+  # end
 
-  Rack::Attack.blocklist('fail2ban pentesters') do |req|
-    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
-    # so the request is blocked
-    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 1, findtime: 10.minutes, bantime: 2.hours) do
-      # The count for the IP is incremented if the return value is truthy
-      CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
-      req.path.include?('/etc/passwd') ||
-      req.path.include?('wp-admin') ||
-      req.path.include?('wp-login') ||
-      req.path.include?('owa') ||
-      req.path.include?('ads.txt') 
-      req.path.include?('php') ||
-      req.path.include?('a2billing') ||
-      req.path.include?('cgi') ||
-      req.path.include?('webdav') ||
-      req.path.include?('ona') ||
-      req.path.include?('inii') ||
-      req.path.include?('sarFILE') ||
-      req.path.include?('global_data') ||
-      req.path.include?('op5config') ||
-      req.path.include?('NonExistence') ||
-      req.path.include?('solr') ||
-      req.path.include?('.well-known') ||
-      req.path.include?('.git') ||
-      req.path.include?('webfig') ||
-      req.path.include?('ads.txt')
-    end
-  end
+  # Rack::Attack.blocklist('fail2ban pentesters') do |req|
+  #   # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+  #   # so the request is blocked
+  #   Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 1, findtime: 10.minutes, bantime: 2.hours) do
+  #     # The count for the IP is incremented if the return value is truthy
+  #     CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+  #     req.path.include?('/etc/passwd') ||
+  #     req.path.include?('wp-admin') ||
+  #     req.path.include?('wp-login') ||
+  #     req.path.include?('owa') ||
+  #     req.path.include?('ads.txt') 
+  #     req.path.include?('php') ||
+  #     req.path.include?('a2billing') ||
+  #     req.path.include?('cgi') ||
+  #     req.path.include?('webdav') ||
+  #     req.path.include?('ona') ||
+  #     req.path.include?('inii') ||
+  #     req.path.include?('sarFILE') ||
+  #     req.path.include?('global_data') ||
+  #     req.path.include?('op5config') ||
+  #     req.path.include?('NonExistence') ||
+  #     req.path.include?('solr') ||
+  #     req.path.include?('.well-known') ||
+  #     req.path.include?('.git') ||
+  #     req.path.include?('webfig') ||
+  #     req.path.include?('ads.txt')
+  #   end
+  # end
 end
