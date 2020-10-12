232a10ad0bf2a5b35f554481611864208379a6e4
archetype2142 <malhotraritwick2011@gmail.com>
2020-10-12 21:28:20 +0200

    cleanup

--- app/controllers/trader_items_controller.rb
+++ app/controllers/trader_items_controller.rb
@@ -1,7 +1,8 @@
 class TraderItemsController < ApplicationController
 
   def index
-
+    order = params[:q][:s].split[1].to_sym || :desc
+    
     active_users ||= User.active
     if params[:q] 
       @search = Item.basic.ransack(params[:q])
@@ -11,9 +12,17 @@ class TraderItemsController < ApplicationController
 
     items ||= @search.result.pluck(:id)
     @item_names ||= Item.where(id: items).pluck(:id, :name)
+    @item_names_only ||= @item_names.map{ |_, name| name }.map
 
     @prices = Kaminari.paginate_array(
-        Price.all.includes([:user]).where(user: active_users, item_id: items).uniq
+        Price.all.includes([:user, :item])
+        .where(
+          user: active_users, 
+          item_id: items
+        )
+        .order(amount: order)
+        .uniq
+
       )
     .page(params[:page])
     .per(params[:per_page])
